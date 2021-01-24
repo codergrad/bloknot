f177f2db482f39ec69a7eaff5431dc7b4cf02ece
RyoukuZen <76735447+RyoukuZen@users.noreply.github.com>
2021-01-24 19:14:26 +0300

    Добавил settings activity.

---
+++ app/src/main/java/codergrad/bloknot/SettingsActivity.java
@@ -0,0 +1,20 @@
+package codergrad.bloknot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+        import com.google.android.material.snackbar.Snackbar;
+
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.widget.Toolbar;
+
+        import android.view.View;
+
+        import android.view.Menu;
+        import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+}
