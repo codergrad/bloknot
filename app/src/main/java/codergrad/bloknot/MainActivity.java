30a416ab7ab9741d0915298b8cde9a7f17fd082a
radaken <alexon249@gmail.com>
2021-01-24 13:10:14 +0300

    Initial Commit

--- app/src/main/java/codergrad/bloknot/MainActivity.java
+++ app/src/main/java/codergrad/bloknot/MainActivity.java
@@ -1,17 +1,24 @@
 package codergrad.bloknot;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.ContactsContract;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +28,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        ArrayList<String> data = new ArrayList<String>();
+        RecyclerView recyclerView = findViewById(R.id.rvNumbers);
+        int numberOfColumns = 3;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        MyRecyclerViewAdapter adapter = new MyRecyclerViewAdapter(this, data);
+        recyclerView.setAdapter(adapter);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            int clickCounter = 0;
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+                db.execSQL("CREATE TABLE IF NOT EXISTS notes(indx INTEGER, title TEXT, content TEXT, date TEXT)");
+                db.execSQL("INSERT INTO notes VALUES (0, 'Sample title', 'Sample Content', '22.01.2021')");
+                Cursor query = db.rawQuery("SELECT * FROM notes", null);
+                String content = "";
+                while(query.moveToNext()) {
+                   content = query.getString(1);
+                }
+                data.add(content);
+                adapter.notifyItemInserted(clickCounter);
+                clickCounter++;
+                query.close();
+                db.close();
             }
+
         });
     }
 
