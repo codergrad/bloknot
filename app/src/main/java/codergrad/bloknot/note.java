479c40067abb1384831fba6ce4c06d80d67cbd79
PersonalPhoenix <applmacter@mail.ru>
2021-02-28 22:21:10 +0300

    Добавил второе поле EditText в ActivityNote

--- app/src/main/java/codergrad/bloknot/note.java
+++
@@ -1,8 +0,0 @@
-package codergrad.bloknot;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class 
