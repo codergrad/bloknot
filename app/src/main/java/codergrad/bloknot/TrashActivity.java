ecc5617eea87027187295c2ba04f67274fbdcb0b
DrillDozer <76707757+DrillDozer@users.noreply.github.com>
2021-04-27 10:01:08 +0300

    Trash activity (#9)
    * добавил вёрстку кнопок и иконки к ним. Изучаю ImageButton
    
    * окончательный вариант кнопок.

---
+++ app/src/main/java/codergrad/bloknot/TrashActivity.java
@@ -0,0 +1,16 @@
+package codergrad.bloknot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trash);
+
+
+    }
+}
